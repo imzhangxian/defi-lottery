a2d5be01044df4807e1bbba4c40a74d4a47accb2
imzhangxian <imzhangxian@gmail.com>
2022-10-18 16:07:24 +0800

    For local deployments

---
+++ deploy/01-deploy.js
@@ -0,0 +1,7 @@
+const { network } = require("hardhat")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+
+}
+
+module.exports.tags = ["all", "mocks"]
--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,44 @@ require("hardhat-gas-reporter")
 require("hardhat-contract-sizer")
 require("dotenv").config()
 
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli.alchemyapi.io/v2/your-api-key"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-    solidity: "0.8.17",
+    defaultNetwork: "hardhat", 
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        },
+        player1: {
+            default: 1,
+        },
+        player2: {
+            default: 2,
+        },
+    },
+    networks: {
+        hardhat: {
+          chainId: 31337
+        },
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            saveDeployments: true,
+            chainId: 5,
+        },
+    }, 
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.17",
+            },
+            {
+                version: "0.4.24",
+            },
+        ],
+    },
 }
